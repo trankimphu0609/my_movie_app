7066496dfa30698d7cd7dc47c6e31a374d683ea2
Phu <tkp06092001@gmail.com>
2026-08-21 07:47:29 +0700

    Update main, constants, pubspec

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
+  static String get apiKey => dotenv.env['TMDB_API_KEY'] ?? '';
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = '0eb29540f0e2c76581d1649a7d57ccda';
   static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/app_route.dart';
 import 'core/app_theme.dart';
 import 'data/repositories/movie_repository.dart';
@@ -9,6 +10,7 @@ import 'presentation/bloc/movie_event.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
